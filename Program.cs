a835291016a95026562bac54263de4b516f311b9
Michael Smith <msmithboise@gmail.com>
2018-09-24 09:15:19 -0600

    commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_morningchallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
